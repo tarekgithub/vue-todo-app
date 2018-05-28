61aeca52fd5da003eebbc29d80acb9cfda89fd03
Tarek C <tarekcourt@protonmail.com>
2018-05-28 22:53:40 +0100

    - importing ToDoList component in main component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from '@/components/TodoList';
+
 export default {
-  name: 'App'
+  components: {
+    // Add a reference to the TodoList component in the components property
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
 }
 </script>
 
